6c3286efc8c588f2c3d5cfefba108c7d76a5ebe4
Sabrina Becker <sbecka8@gmail.com>
2020-05-23 15:12:24 -0400

    Create initial sections of mock page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,56 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HI</h1>
+    <div className='App'>
+      <header className='header'>
+        <nav className='top-product-nav'></nav>
+        <nav className='main-nav'></nav>
       </header>
+      <div className='hero'>
+        <h1 className='hero-text'>The best personal training, right in your own home</h1>
+        <button className='hero-button'>JOIN iFIT COACH</button>
+      </div>
+      <div className='media-slider'>
+        <button className='left-arrow-button'> &lt; </button>
+        <div className='media-testimonial'></div>
+        <div className='media-testimonial'></div>
+        <div className='media-testimonial'></div>
+        <div className='media-testimonial'></div>
+        <div className='media-testimonial'></div>
+        <button className='right-arrow-button'> &gt; </button>
+      </div>
+      <div className='grid-library-cards'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className='equipment-section'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <footer>
+        <div className='website-links'>
+          <div className='company-links'></div>
+          <div className='account-links'></div>
+          <div className='support-links'></div>
+        </div>
+        <div className='social-links'>
+          
+        </div>
+        <div className='language-legal-info'>
+          <select className='language-selector'>English</select>
+          <div className='legal-info'>
+
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
