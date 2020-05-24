66b1097a8b2bda2aa3d3b689e3467f5c11e58cc0
Sabrina Becker <sbecka8@gmail.com>
2020-05-24 18:39:53 -0400

    Add responsive styles for header and nav bars

--- src/MainNav.js
+++ src/MainNav.js
@@ -2,14 +2,21 @@
 import React from 'react';
 
 function MainNav() {
+    
     return (
         <nav className='main-nav'>
-            <img src={require('./images/ifitcoachlogo.png')} className='ifitcoach-logo' alt='iFit Coach logo'></img>
-            <div>
-                <a>EXERCISE</a>
-                <a>NUTRITION</a>
-                <a>ACTIVITY</a>
-                <a>SLEEP</a>
+            <div className='logo-and-menu-burger'>
+               <img src={require('./images/ifitcoachlogo.png')} className='ifitcoach-logo' alt='iFit Coach company logo' />
+                <div className="menu-burger">
+                    <span className="menu-burger-btn"></span>
+                </div> 
+            </div>
+            
+            <div className='main-links'>
+                <a href='#'>EXERCISE</a>
+                <a href='#'>NUTRITION</a>
+                <a href='#'>ACTIVITY</a>
+                <a href='#'>SLEEP</a>
             </div>
             <button className='sign-up-button'>SIGN UP</button>
         </nav>
