54d6f5db85db846fbd4a502f1cb506f6fc838f2f
Sabrina Becker <sbecka8@gmail.com>
2020-05-24 13:40:28 -0400

    Add content and styles for equipment selection

--- src/EquipmentSelection.js
+++ src/EquipmentSelection.js
@@ -1,26 +1,28 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 
 function EquipmentSelection() {
     return (
         <div className='equipment-selection'>
             <h1 className='equipment-selection-title'>Interested in our exciting iFit-enabled equipment?</h1>
-            <div className='equipment-card'>
-            <img />
-            <h2 className='equipment-title'>Treadmills</h2>
+            <div className='equipment'>
+                <div className='equipment-card'>
+                    <img src={require('./images/treadmill.png')} alt='Woman working out on treadmill.' />
+                    <h2 className='equipment-title'>Treadmills</h2>
+                </div>
+                <div className='equipment-card'>
+                    <img src={require('./images/bikeperson.png')} alt='Man working out on stationary bike.' />
+                    <h2 className='equipment-title'>Bikes</h2>
+                </div>
+                <div className='equipment-card'>
+                    <img src={require('./images/ellipticalperson.png')} alt='Woman working out on eliptical.' />
+                    <h2 className='equipment-title'>Ellipticals</h2>
+                </div>
+                <div className='equipment-card'>
+                    <img src={require('./images/strengthperson.png')} alt='Man working out on weight machine.' />
+                    <h2 className='equipment-title'>Strength</h2>
+                </div>                
             </div>
-            <div className='equipment-card'>
-            <img />
-            <h2 className='equipment-title'>Bikes</h2>
-            </div>
-            <div className='equipment-card'>
-            <img />
-            <h2 className='equipment-title'>Ellipticals</h2>
-            </div>
-            <div className='equipment-card'>
-            <img />
-            <h2 className='equipment-title'>Strength</h2>
-            </div>
+
         </div>
     );
 }
